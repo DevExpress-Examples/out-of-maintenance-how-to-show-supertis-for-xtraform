2f62ec8e739009eeed009752190bab5059230754
Svetlana Peksheva (DevExpress) <nikishina@corp.devexpress.com>
2019-11-12 20:30:24 +0300

    add VB

---
+++ VB/DXApplication5/Main.vb
@@ -0,0 +1,12 @@
+﻿Imports System
+Imports System.Linq
+
+Namespace DXSample
+	Partial Public Class Main
+		Inherits XtraFormEx
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
---
+++ VB/DXApplication5/Program.vb
@@ -0,0 +1,19 @@
+﻿Imports DevExpress.UserSkins
+Imports System
+Imports System.Linq
+Imports System.Windows.Forms
+
+Namespace DXSample
+	Friend Module Program
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		<STAThread>
+		Sub Main()
+			BonusSkins.Register()
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			Application.Run(New Main())
+		End Sub
+	End Module
+End Namespace
